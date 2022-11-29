f5c7f44a78a90773274c4f7453dd9605da453f91
PratikBansod-Eptura <pratik.bansod@eptura.com>
2022-11-29 17:49:47 +0530

    api_view and its url created for blogs list

--- blog_system/blog/views.py
+++ blog_system/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Blog
+from .serializers import BlogSerializer
+
 
 # Create your views here.
+@api_view()
+def blog_list(request):
+    if request.method == 'GET':
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        print(f'\nserilizer data = {serializer.data}\n')
+        return Response(serializer.data)
