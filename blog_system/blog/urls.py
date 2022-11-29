14e95d0d57a74bab4fd1a553c07974e74c0391f7
PratikBansod-Eptura <pratik.bansod@eptura.com>
2022-11-29 18:10:36 +0530

    api view and its url created for creating blog

--- blog_system/blog/urls.py
+++ blog_system/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import blog_list
+from .views import blog_list, blog_create
 
 urlpatterns = [
-    path('blogs/', blog_list, name='blogs')
+    path('blogs/', blog_list, name='blogs'),
+    path('blog-create/', blog_create, name='blog-create')
 ]
--- blog_system/blog/views.py
+++ blog_system/blog/views.py
@@ -3,13 +3,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Blog
 from .serializers import BlogSerializer
-
+from rest_framework import status
 
 # Create your views here.
 @api_view()
-def blog_list(request):
+def blog_list(request, id = None):
     if request.method == 'GET':
         blogs = Blog.objects.all()
         serializer = BlogSerializer(blogs, many=True)
         print(f'\nserilizer data = {serializer.data}\n')
-        return Response(serializer.data)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+@api_view(['POST'])
+def blog_create(request):
+    if request.method == 'POST':
+        serializer = BlogSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {'message':'Blog created successfully'}
+            return Response(response, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
