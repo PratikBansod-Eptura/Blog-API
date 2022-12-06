fb26327cb0e88b53eb61fe04c52a2ea13869fc23
PratikBansod-Eptura <pratik.bansod@eptura.com>
2022-12-06 12:46:21 +0530

    79 char length and 2 empty lines between function maintained as per PEP8 in serializers.py

--- blog_system/blog/serializers.py
+++ blog_system/blog/serializers.py
@@ -5,7 +5,8 @@ from .models import Blog, CustomUser
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
-        fields = ['id','user', 'title', 'description', 'published_date', 'last_updated_on', 'approval_status',]
+        fields = ['id','user', 'title', 'description', 'published_date',
+                  'last_updated_on', 'approval_status',]
         read_only_fields = ('user','approval_status')
 
     ## To show fields of user in response insted of user id
@@ -25,7 +26,9 @@ class BlogSerializer(serializers.ModelSerializer):
         published_date = validated_data.get('published_date')
         last_updated_on = validated_data.get('last_updated_on')
 
-        blog = Blog(title=title, description=description, published_date=published_date, last_updated_on=last_updated_on)
+        blog = Blog(title=title, description=description,
+                    published_date=published_date,
+                    last_updated_on=last_updated_on)
         blog.user = user_obj
         blog.save()
         return blog
@@ -35,7 +38,8 @@ class CustomUserSignupSerializer(serializers.ModelSerializer):
     #confirm_password = serializers.CharField(style={'input_type':'password'}, write_only=True)
     class Meta:
         model = CustomUser
-        fields = ['id','username', 'password', 'email', 'gender', 'mob_number', 'city']
+        fields = ['id','username', 'password',
+                  'email', 'gender', 'mob_number', 'city']
 
     def create(self, validated_data):
         """
@@ -48,7 +52,8 @@ class CustomUserSignupSerializer(serializers.ModelSerializer):
         mob_number = validated_data['mob_number']
         city = validated_data['city']
 
-        user = CustomUser(username=username, email=email, gender=gender, mob_number=mob_number, city=city)
+        user = CustomUser(username=username, email=email, gender=gender,
+                          mob_number=mob_number, city=city)
         user.set_password(password)
         user.save()
         return user
